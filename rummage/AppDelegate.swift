aa07790354a9e449c6fc3f10d8b87bf20ffb1482
Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>
2025-01-03 05:33:04 -0800

    swift rust interop

--- rummage/AppDelegate.swift
+++ rummage/AppDelegate.swift
@@ -5,7 +5,7 @@
 //  Created by Vincent Zhao on 1/3/25.
 //
 
-import UIKit
+import UIKit;
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         return true
     }
 
---
+++ rummage/Test.m
@@ -0,0 +1,8 @@
+//
+//  Test.m
+//  rummage
+//
+//  Created by Vincent Zhao on 1/3/25.
+//
+
+#import <Foundation/Foundation.h>
